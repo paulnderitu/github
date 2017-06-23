aa385690cdbc94bb9805e7c698092140791a8619
Your Name <youremail@domain.com>
2017-06-23 10:32:25 +0300

    introducing the github lookup apiKey to request.js

--- js/request.js
+++ js/request.js
@@ -1,8 +1,10 @@
 
-exports.getRepos = function(){
+var apiKey = require('./../.env').apiKey;
   $.get('https://api.github.com/users/' +name + '/repos?access_token=' + apiKey).then(function(repos){
     displayFunction(repos);
   }).fail(function(error){
     $('.showUser').text("Username" + name + "repos" + error.responseJSON.message + "." + "Enter a valid github Username");
   });
 };
+
+exports.userModule = User;
