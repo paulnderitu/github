b911732298377bd10d9c01c5e4aece6c0bb42d86
Your Name <youremail@domain.com>
2017-06-23 11:28:09 +0300

    required the request.js actipn on the github-interface. js

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,12 +1,7 @@
-var apiKey = require('./../.env').apiKey;
-
-$(document).ready(function() {
-  $('#usernameRepos').click(function() {
-    var username = $('#username').val();
-    $('#username').val("");
-    $('.showrepos').text("The username you entered is " + username + ".");
-    $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response);
+var User = require('./../js/request.js').userModule;
+var showRepositories = function (repos) {
+  $('#showrepos').empty();
+  repos.forEach(function(repo) {
+    $('#showrepos').append("<li><a href='"+ repo.html_url + "'>" + repo.name + "</a>: " + repo.desription + "</li>");
   });
-  });
-});
+};
